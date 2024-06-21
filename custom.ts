9c83284c8208d7e396402f971deeb6d0ccb3bcad
mgismissing <130852204+mgismissing@users.noreply.github.com>
2024-06-21 12:05:11 +0200

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -15,12 +15,23 @@ enum MyEnum {
  * Custom blocks
  */
 //% weight=10 color=#0fbc11 icon="\uf233"
+//% groups='["Create", "Modify"]'
 namespace requests {
     /**
      * Initializes the Radio module. Nothing will work before this block is called.
      */
-    //% blockId=radio_init block="Initialize Radio module"
+    //% blockId=radio_init
+    //% block="Initialize Radio module"
+    //% group="Create"
     export function radio_init(): void {
         sdwireless.sdw_init()
     }
+
+    /**
+     * Returns a new Request object.
+     */
+    //% blockId=request block="request url %url to radio address %radio_address"
+    //export function request(radio_address: number, url: string): void {
+        
+    //}
 }
